54745a991c275a5c9b59a05c285479039bdf0ea9
DenysMaksymov <koksdns123@gmail.com>
2021-01-29 15:04:37 +0200

    add data

---
+++ lib/terminal_debug.dart
@@ -0,0 +1,35 @@
+library terminal_debug;
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+
+class TerminalDataSender{
+  String iPLocalAddress;
+  String dataForSend;
+
+  TerminalDataSender({
+   @required this.dataForSend,
+   @required this.iPLocalAddress
+});
+
+  String path = 'file.txt';
+  Map jsonData;
+
+  Future sendData()async{
+    jsonData = {
+      'time': new DateTime.now(),
+      'body': dataForSend,
+    };
+    try{
+      HttpClientRequest request = await HttpClient().post(iPLocalAddress, 4040,path)
+        ..headers.contentType = ContentType.json
+        ..write(jsonEncode(jsonData));
+      HttpClientResponse response = await request.close();
+      await utf8.decoder.bind(response).forEach(print);
+    }catch(error){
+      print(error);
+    }
+  }
+
+}
